dce415c1ce3cffda952d7e5b2d29da3a86aa203e
JOSUE_SU <jsumare@unsa.edu.pe>
2024-04-23 09:53:13 -0500

    File creation

---
+++ Laboratorio_01_Sumare_Josue.py
@@ -0,0 +1,3 @@
+# • Implementar un algoritmo que indique si es una cadena de ADN, ARN o proteínas.
+# • Implementar algoritmos para la transcripción de cadenas (de ADN a ARN).
+# • Para las pruebas, usar el archivo de pruebas que se encuentra en el classroom.
